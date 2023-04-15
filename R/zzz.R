61321bb27726d6e92836a3ac4dd6de6b2b2a5a5d
Koen Derks <koen-derks@hotmail.com>
2023-04-15 08:22:16 +0200

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -21,8 +21,8 @@
   sysfonts::font_add_google("Rampart One")
   sysfonts::font_add_google("Fredericka the Great")
   sysfonts::font_add_google("Dancing Script")
-  #sysfonts::font_add_google("Walter Turncoat")
-  try({
+  sysfonts::font_add_google("Walter Turncoat")
+  #try({
     sysfonts::font_add("American Uncial Regular", system.file("fonts", "uncial.otf", package = "rcityviews"))
     sysfonts::font_add("Rage", system.file("fonts", "rage.ttf", package = "rcityviews"))
   #})
